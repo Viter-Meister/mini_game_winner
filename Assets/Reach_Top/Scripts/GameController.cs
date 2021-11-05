fc9ab7424229a491d43aed7ca7b8407558d45ba1
Viter Viktor <viter02@inbox.ru>
2021-11-05 15:07:01 +0300

    Add functional Win

--- Assets/Reach_Top/Scripts/GameController.cs
+++ Assets/Reach_Top/Scripts/GameController.cs
@@ -1,17 +1,92 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    private BoxePos nowBox = new BoxePos(-7.5f, -3.65f);
     public float BoxeChangePlaceSpeed = 0.5f;
+    public Transform BoxeToPlace;
+    int i = 0, j = 4;
+    float oldY = -1, newY = -1;
 
-    private List<Vector2> AllBoxePosition = new List<Vector2>();
+    public GameObject BoxeToCreateBlue, AllBoxesBlue;
+
+    private bool IsWin;
+
+    public Text Winner;
+
+    public float TimerTime = 3;
+    public Text TimerText;
+    private bool FlagForTimmer = false;
+
+    private Vector2[] AllSpawnPositionBlue = new Vector2[6] {
+        new Vector2(-8, -4),
+        new Vector2(-7, -4),
+        new Vector2(-6, -4),
+        new Vector2(-5, -4),
+        new Vector2(-4, -4),
+        new Vector2(-3, -4),
+    };
+
+    private int[,] AllHeldPosition = new int[7, 6]
+    {
+        {0, 0, 0, 0, 0, 0 },
+        {0, 0, 0, 0, 0, 0 },
+        {0, 0, 0, 0, 0, 0 },
+        {0, 0, 0, 0, 0, 0 },
+        {0, 0, 0, 0, 0, 0 },
+        {0, 0, 0, 0, 0, 0 },
+        {0, 0, 0, 0, 0, 0 },
+    };
+
+    private Coroutine showBoxePlace;
 
     private void Start()
     {
-        StartCoroutine(ShowBoxePlace());
+        showBoxePlace = StartCoroutine(ShowBoxePlace());
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && BoxeToPlace != null)
+        {
+#if !UNITY_EDITOR
+            if (Input.GetTouch(0).phase != TouchPhase.Began)
+                return;
+#endif
+            GameObject NewBoxe = Instantiate(BoxeToCreateBlue,
+            BoxeToPlace.position,
+            Quaternion.identity);
+            NewBoxe.transform.SetParent(AllBoxesBlue.transform);
+
+            TakePosition(NewBoxe.transform.position);
+
+            for (int i = 0; i < 7; i++)
+                for (int j = 0; j < 6; j++)
+                    if (AllHeldPosition[i, j] == 1)
+                    {
+                        newY = i;
+                        break;
+                    }
+
+            if (newY > oldY)
+            {
+                for (int i = 0; i < 6; i++)
+                    AllSpawnPositionBlue[i].y += 1;
+                oldY = newY;
+            }
+
+            SpawnPosition();
+        }
+
+        if (!IsWin && newY == 6)
+        {
+            IsWin = true;
+            StopCoroutine(showBoxePlace);
+            Destroy(BoxeToPlace.gameObject);
+            Winner.text = "Blue won!";
+        }
     }
 
     IEnumerator ShowBoxePlace()
@@ -26,40 +101,89 @@ public class GameController : MonoBehaviour
 
     private void SpawnPosition()
     {
-        List<Vector2> positions = new List<Vector2>();
-        float x = -7.5f;
-        
+        if (i <= 5)
+        {
+            BoxeToPlace.position = AllSpawnPositionBlue[i];
+            i += 1;
+        }
+        else if (j > 0)
+        {
+            BoxeToPlace.position = AllSpawnPositionBlue[j];
+            j -= 1;
+        }
+        else
+        {
+            i = 0;
+            j = 4;
+            BoxeToPlace.position = AllSpawnPositionBlue[i];
+            i += 1;
+        }
     }
 
-    private bool IsPositionEmpty(Vector2 targetPos)
+    private void TakePosition(Vector2 pos)
     {
-        if (targetPos.y < -3.65)
-            return false;
-        foreach (var pos in AllBoxePosition)
-            if (pos.x == targetPos.x && pos.y == targetPos.y)
-                return false;
-        return true;
-    }
-}
-
-struct BoxePos
-{
-    public float x, y;
-
-    public BoxePos(float x, float y)
-    {
-        this.x = x;
-        this.y = y;
-    }
-
-    public Vector2 getVector()
-    {
-        return new Vector2(x, y);
-    }
-
-    public void setVector(Vector2 pos)
-    {
-        x = pos.x;
-        y = pos.y;
+        switch (pos.x)
+        {
+            case -8:
+                {
+                    for (int i = 0; i < 7; i++)
+                        if (AllHeldPosition[i, 0] == 0)
+                        {
+                            AllHeldPosition[i, 0] = 1;
+                            break;
+                        }
+                    break;
+                }
+            case -7:
+                {
+                    for (int i = 0; i < 7; i++)
+                        if (AllHeldPosition[i, 1] == 0)
+                        {
+                            AllHeldPosition[i, 1] = 1;
+                            break;
+                        }
+                    break;
+                }
+            case -6:
+                {
+                    for (int i = 0; i < 7; i++)
+                        if (AllHeldPosition[i, 2] == 0)
+                        {
+                            AllHeldPosition[i, 2] = 1;
+                            break;
+                        }
+                    break;
+                }
+            case -5:
+                {
+                    for (int i = 0; i < 7; i++)
+                        if (AllHeldPosition[i, 3] == 0)
+                        {
+                            AllHeldPosition[i, 3] = 1;
+                            break;
+                        }
+                    break;
+                }
+            case -4:
+                {
+                    for (int i = 0; i < 7; i++)
+                        if (AllHeldPosition[i, 4] == 0)
+                        {
+                            AllHeldPosition[i, 4] = 1;
+                            break;
+                        }
+                    break;
+                }
+            case -3:
+                {
+                    for (int i = 0; i < 7; i++)
+                        if (AllHeldPosition[i, 5] == 0)
+                        {
+                            AllHeldPosition[i, 5] = 1;
+                            break;
+                        }
+                    break;
+                }
+        }
     }
 }
