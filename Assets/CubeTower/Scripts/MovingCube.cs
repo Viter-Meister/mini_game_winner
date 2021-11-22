dc41489ec8c44ec3b9b039a237eb5cd90aa47bd4
Roman Burdukovskii <burdukovskii@sfedu.ru>
2021-11-22 22:15:15 +0300

    added functionality for cutting and falling cube

--- Assets/CubeTower/Scripts/MovingCube.cs
+++ Assets/CubeTower/Scripts/MovingCube.cs
@@ -5,18 +5,51 @@ using UnityEngine;
 public class MovingCube : MonoBehaviour
 {
     public static MovingCube CurrentCube { get; private set; }
+    public static MovingCube LastCube { get; private set; }
 
     [SerializeField]
-    private float moveSpeed = 2f;
+    private float moveSpeed = 3f;
 
     private void OnEnable()
     {
+        if (LastCube == null)
+            LastCube = GameObject.Find("Start").GetComponent<MovingCube>();
+
         CurrentCube = this;
     }
 
     internal void Stop()
     {
         moveSpeed = 0;
+        float hangover = transform.position.z - LastCube.transform.position.z;
+
+        float direction = hangover > 0 ? 1f : -1f;
+        SplitCubeOnZ(hangover, direction);
+    }
+
+    private void SplitCubeOnZ(float hangover, float direction)
+    {
+        float newSize = LastCube.transform.localScale.z - Mathf.Abs(hangover);
+        float fallingBlockSize = transform.localScale.z - newSize;
+
+        float newZPosition = LastCube.transform.position.z + (hangover / 2);
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, newSize);
+        transform.position = new Vector3(transform.position.x, transform.position.y, newZPosition);
+
+        float cubeEdge = transform.position.z + newSize / 2f * direction;
+        float fallingBlockZPosition = cubeEdge + fallingBlockSize / 2f * direction;
+
+        SpawnDropCube(fallingBlockZPosition, fallingBlockSize);
+    }
+
+    private void SpawnDropCube(float fallingBlockZPosition, float fallingBlockSize)
+    {
+        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, fallingBlockSize);
+        cube.transform.position = new Vector3(transform.position.x, transform.position.y, fallingBlockZPosition);
+
+        cube.AddComponent<Rigidbody>();
+        Destroy(cube.gameObject, 2f);
     }
 
     private void Update()
