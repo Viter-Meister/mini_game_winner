5df5c41e3d0bbf0ff7e4dfb0d2be8a98037d87a2
Kirill Koksharov <koksharov-kir@yandex.ru>
2021-11-24 19:52:08 +0300

    Improved BoardGame

--- Assets/Games/Board/Scripts/BoardDice.cs
+++ Assets/Games/Board/Scripts/BoardDice.cs
@@ -3,13 +3,18 @@ using UnityEngine;
 
 public class BoardDice : MonoBehaviour
 {
-    void Start()
+    public BoardMain main;
+    public Transform UpSide;
+    public Transform[] sides;
+
+    void OnEnable()
     {
         StartCoroutine(RollTheDice());
     }
 
     private IEnumerator RollTheDice()
     {
+        yield return new WaitForSeconds(1);
         float speed;
         for (int i = 1; i < 25; i++)
         {
@@ -17,5 +22,11 @@ public class BoardDice : MonoBehaviour
             transform.Rotate(new Vector3(Random.Range(0, 4) * 90, Random.Range(0, 4) * 90, Random.Range(0, 4) * 90));
             yield return new WaitForSeconds(speed);
         }
+        yield return new WaitForSeconds(1);
+        for (int i = 0; i < sides.Length; i++)
+            if (Vector3.Distance(sides[i].position, UpSide.position) < 0.2f)
+                main.FromDice(i);
+            
+        gameObject.SetActive(false);
     }
 }
